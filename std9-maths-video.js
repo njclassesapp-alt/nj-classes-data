9324bebe561d3cff7086a807b1ed5131ee1f382f
Nj Classes <njclassesapp@gmail.com>
2026-04-28 19:13:32 +0530

    Create std9-maths-video.js

---
+++ std9-maths-video.js
@@ -0,0 +1,72 @@
+njVideoDatabase = {
+
+  // ==========================================
+// Std 9 Maths - Premium YouTube Video Database
+// ==========================================
+
+const std9VideoDatabase = {
+    // ------------------------------------
+    // પ્રકરણ 1: સંખ્યા પદ્ધતિ
+    // ------------------------------------
+    1: [
+        { id: "AOp1gZXMh3o", title: "સૂત્ર એન્ડ થીયરી" },
+        { id: "w8Io6uUaaKg", title: "સ્વાધ્યાય 1.1 - Q 1" },
+        { id: "nHOxAzKKaQg", title: "સ્વાધ્યાય 1.2 - Q 1" },
+        { id: "j9ZwbMO3C1I", title: "સ્વાધ્યાય 1.2 - Q 3" },
+        { id: "hI1RBC3ClUQ", title: "સ્વાધ્યાય 1.3 - Q 1" },
+        { id: "nk6cOgn55g4", title: "સ્વાધ્યાય 1.3 - Q 3" },
+        { id: "KykzWi27ftQ", title: "સ્વાધ્યાય 1.3 - Q 4" },
+        { id: "gl73QL-sqAU", title: "સ્વાધ્યાય 1.3 - Q 5" },
+        { id: "WZYPirfq0Sk", title: "સ્વાધ્યાય 1.3 - Q 6 & 8" },
+        { id: "DUQUiZUiMFQ", title: "સ્વાધ્યાય 1.3 - Q 9" },
+        { id: "9Fpq1hpB1uo", title: "સ્વાધ્યાય 1.4 - Q 1" },
+        { id: "xSphpfRiRXc", title: "સ્વાધ્યાય 1.4 - Q 2" },
+        { id: "_TlA8A6fcAA", title: "સ્વાધ્યાય 1.4 - Q 3" },
+        { id: "ZejUvv_fuPM", title: "સ્વાધ્યાય 1.4 - Q 4" },
+        { id: "KZQWd4dMuwI", title: "સ્વાધ્યાય 1.4 - Q 5" },
+        { id: "j3QL_6MikB8", title: "ઘાત, ઘાતાંકના નિયમો" },
+        { id: "ammNEXL9pg0", title: "સ્વાધ્યાય 1.5 - Q 1 & 3" }
+    ],
+
+    // ------------------------------------
+    // પ્રકરણ 2: બહુપદીઓ
+    // ------------------------------------
+    2: [
+        { id: "9AU9VElneNY", title: "થીયરી એન્ડ સૂત્ર" },
+        { id: "cI57MEB86W4", title: "સ્વાધ્યાય 2.1 - Q 1" },
+        { id: "LLyA_4hwtTE", title: "સ્વાધ્યાય 2.1 - Q 2 થી 5" },
+        { id: "nS5JkUjM5VE", title: "સ્વાધ્યાય 2.2 - Q 1" },
+        { id: "vdHF3yf4YPE", title: "સ્વાધ્યાય 2.2 - Q 2 (i, ii)" },
+        { id: "k8TdxOwYXlc", title: "સ્વાધ્યાય 2.2 - Q 2 (iii, iv)" },
+        { id: "zDyRCYoNVXI", title: "સ્વાધ્યાય 2.2 - Q 3 (i થી iv)" },
+        { id: "_-nZa9sLBwQ", title: "સ્વાધ્યાય 2.2 - Q 3 (v થી viii)" },
+        { id: "MFgXwM6ZpI8", title: "સ્વાધ્યાય 2.2 - Q 4" },
+        { id: "0G90DZnvF3M", title: "સ્વાધ્યાય 2.3 - Q 1" },
+        { id: "_d6lcIvQGhQ", title: "સ્વાધ્યાય 2.3 - Q 2" },
+        { id: "Z4QE_dew3gk", title: "સ્વાધ્યાય 2.3 - Q 3" },
+        { id: "Ct2C55j2Glc", title: "સ્વાધ્યાય 2.3 - Q 4 (i, ii)" },
+        { id: "Bw70_7JxvgQ", title: "સ્વાધ્યાય 2.3 - Q 4 (iii, iv)" },
+        { id: "_OSq7AEDJEg", title: "સ્વાધ્યાય 2.3 - Q 5 (i, ii)" },
+        { id: "vx_8WH_Tzak", title: "સ્વાધ્યાય 2.3 - Q 5 (iii, iv)" },
+        { id: "7cfW_t-QZfU", title: "નિત્યસમ સમજ" },
+        { id: "QEbyIRBvArM", title: "સ્વાધ્યાય 2.4 - Q 1" },
+        { id: "m6bEslCMvTk", title: "સ્વાધ્યાય 2.4 - Q 2" },
+        { id: "1S-XyM9c4yg", title: "સ્વાધ્યાય 2.4 - Q 3" },
+        { id: "POGtwG2C7I8", title: "સ્વાધ્યાય 2.4 - Q 4 (i, ii, iii)" },
+        { id: "U3UzB41pHm0", title: "સ્વાધ્યાય 2.4 - Q 4 (iv થી vi)" },
+        { id: "L--mD_wy6rI", title: "સ્વાધ્યાય 2.4 - Q 5" },
+        { id: "GIs5NH5hq68", title: "સ્વાધ્યાય 2.4 - Q 6 (i, ii)" },
+        { id: "pxtNdDaA-Ss", title: "સ્વાધ્યાય 2.4 - Q 6 (iii, iv)" },
+        { id: "y_rvzdkgDZ4", title: "સ્વાધ્યાય 2.4 - Q 7" },
+        { id: "3kaHP_qvZYI", title: "સ્વાધ્યાય 2.4 - Q 8 (i, ii, iii)" },
+        { id: "KmPg2dsSpzU", title: "સ્વાધ્યાય 2.4 - Q 8 (iv, v)" },
+        { id: "hqSFr4bE_kA", title: "સ્વાધ્યાય 2.4 - Q 9" },
+        { id: "cAhSh_9ukvo", title: "સ્વાધ્યાય 2.4 - Q 10" },
+        { id: "UT186caYiSA", title: "સ્વાધ્યાય 2.4 - Q 11" },
+        { id: "R7n4Ptiirzg", title: "સ્વાધ્યાય 2.4 - Q 12" },
+        { id: "-F0rHYrCdy0", title: "સ્વાધ્યાય 2.4 - Q 13 & 14" },
+        { id: "1h3OWOoWZaU", title: "સ્વાધ્યાય 2.4 - Q 15" },
+        { id: "IPHSrMmk9-M", title: "સ્વાધ્યાય 2.4 - Q 16" }
+    ]
+};
+         
